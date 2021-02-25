6d4a2bcd94eef21a607e89dd8b49341c214b46d9
Roxana Tot <roxana.tot@gmail.com>
2021-02-25 15:20:01 +0000

    Character jumps

--- Heroes Journey/Assets/Scripts/Move.cs
+++ Heroes Journey/Assets/Scripts/Move.cs
@@ -12,32 +12,24 @@ public class Move : MonoBehaviour
 
     SpriteRenderer sp;  // variable of class SpriteRenderer
 
-
-    private void Awake(){
-        {
-            rb = GetComponent<Rigidbody2D>();      // Rigidbody is attached to the player using the rb variable
-            sp = GetComponent<SpriteRenderer>();    // Gives us access to spriterenderer
-        }
-    }
- 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         
     }
 
+    private void Awake(){  // run once, and instantiates objects
+            rb = GetComponent<Rigidbody2D>();      // Rigidbody is attached to the player using the rb variable
+            sp = GetComponent<SpriteRenderer>();    // Gives us access to spriterenderer
+    }
+
     private void FixedUpdate()  // updates with every frame
     {
          xInput = Input.GetAxis("Horizontal");          // getting input from horizontal axis (L or R arrow key or A and D)
-         yInput = Input.GetAxis("Vertical");            //getting input from vertical axis (up or down arrow key or w and s)
+         //yInput = Input.GetAxis("Vertical");            //getting input from vertical axis (up or down arrow key or w and s)
 
-         transform.Translate(xInput * moveSpeed, yInput * moveSpeed, 0);   // applying the user defined speed to the input in x and y (i.e. left arrow move and applying 5 movespeed to that)
+        //  transform.Translate(xInput * moveSpeed, yInput * moveSpeed, 0);   // applying the user defined speed to the input in x and y (i.e. left arrow move and applying 5 movespeed to that)
+         transform.Translate(xInput * moveSpeed, 0, 0);   // applying the user defined speed to the input in x and y (i.e. left arrow move and applying 5 movespeed to that)
 
          PlatformMover();
          FlipPlayer();
