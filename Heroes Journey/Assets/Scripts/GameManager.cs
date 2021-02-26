0561059d19e787bf3e215b15af7ee74ed8166dc6
Steve Cadden <s.p.cadden@gmail.com>
2021-02-26 14:52:22 +0000

    working on endgame

---
+++ Heroes Journey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completedLevelUI;
+
+    public void completeLevel()
+    {
+        if (ScoreScript.scoreValue == 1){
+        completedLevelUI.SetActive(true);
+        }
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false){
+            {
+                gameHasEnded=true;
+                Debug.Log("game won");
+            }
+        }
+    }
+
+}
