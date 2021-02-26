824605776271a7d24968d067b79d0af0cd83be03
Stella Taylor <stellataylor10@gmail.com>
2021-02-26 12:33:29 +0000

    trying to add spikes

---
+++ Heroes Journey/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+
+    public int currentHealth;
+    public int maxHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+void Die() {
+    UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+}
+    // Update is called once per frame
+    void Update()
+    {
+        // if(currentHealth>maxHealth){
+        //     currentHealth = maxHealth;
+        // }
+
+        if(currentHealth<=0){
+            Die();
+        }
+    }
+
+    public void Damage(int dmg){
+        currentHealth -= dmg;
+    }
+}
---
+++ Heroes Journey/Assets/Scripts/Spikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+
+   private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       player = GameObject.FindGameObjectsWithTag("Player").GetComponent<Player>();
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+       if(col.CompareTag("Player")){
+          player.Damage(100);
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
