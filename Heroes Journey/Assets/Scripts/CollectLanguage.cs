9aa399cfd3a8ec024f4165db335e901e3310d289
Roxana Tot <roxana.tot@gmail.com>
2021-02-26 11:53:49 +0000

    Score added

---
+++ Heroes Journey/Assets/Scripts/CollectLanguage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectLanguage : MonoBehaviour
+{
+    public int score = 0;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Language"))
+        {
+            score += 1;
+            Debug.Log(score);
+        }
+    }
+
+}
